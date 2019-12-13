49f2047677b1d1f8d749925f4ab7a0fd36d6bc72
wat4m <wat4m@foxmail.com>
2019-12-13 18:21:14 +0800

    new file:   base.py 	new file:   basesql.py

---
+++ base.py
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+class injection():
+    def __init__(self, base_url):
+        self.base_url = base_url
+
+    def check(self):
+        pass
+
+    def order_by(self):
+        pass
+
+    def union_select_url(self):
+        pass
+
+    def injection_dbname(self):
+        pass
+
+    def injection_tablename(self):
+        pass
+
+    def injection_columns(self):
+        pass
+
+    def dump(self):
+        pass
