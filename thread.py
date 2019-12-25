ef5b04a499c4d5ea604949cb9b13c7f23e704503
wat4m <wat4m@foxmail.com>
2019-12-25 17:41:55 +0800

    update

--- thread.py
+++ thread.py
@@ -1,18 +1,27 @@
 # coding:utf-8
-import random
-from threading import Thread
-from time import sleep
+import wx
+import win32api
+import sys,os
 
-def action(max):
-    for i in range(max):
-        print("当前线程:%s" %i)
-        sleep(1)
+APP_TITLE = u'launch status'
 
-def main():
-    Thread(target=action, args=(10,))
-    for i in range(20):
-        threads=Thread(target=action,args=(10,))
-        threads.start()
-        threads.join()
+class mainFrame(wx.Frame):
+    def __init__(self):
+        wx.Frame.__init__(self,None,-1,APP_TITLE,style=wx.DEFAULT_FRAME_STYLE)
+        self.SetBackgroundColour(wx.Colour(224,224,224))
+        self.SetSize((800,600))
+        self.Center()
+
+        wx.StaticText(self,-1,u'第一行输入框',pos=(50,100),size=(100,-1),style=wx.ALIGN_RIGHT)
+
+
+class mainApp(wx.App):
+    def OnInit(self):
+        self.SetAppName(APP_TITLE)
+        self.Frame = mainFrame()
+        self.Frame.Show()
+        return  True
+
 if __name__ == '__main__':
-    main()
+    app = mainApp(redirect=True,filename="debug.txt")
+    app.MainLoop()
