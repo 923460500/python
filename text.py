da120613c0257ea1220888f8f78b1d461a5b6f0b
wat4m <wat4m@foxmail.com>
2019-12-30 18:27:56 +0800

    update

--- text.py
+++ text.py
@@ -5,43 +5,60 @@ import os
 
 class MainWindow(wx.Frame):
     def __init__(self,parent,title):
-        wx.Frame.__init__(self,parent,title=title,size=(500,200))
+        self.dirname = ''
+        wx.Frame.__init__(self, parent , title=title ,size =(200,-1) )
         self.control = wx.TextCtrl(self,style = wx.TE_MULTILINE)
         self.CreateStatusBar()
-        tb = wx.ToolBar(self,wx.ID_ANY)
-        self.ToolBar = tb
 
         filemenu = wx.Menu()
 
-
-        menu_about = filemenu.Append(wx.ID_ABORT,u'关于',u'关于程序的信息')
+        menuOpen = filemenu.Append(wx.ID_OPEN,'&OEPN','OPEN A FILE')
+        menuAbout = filemenu.Append(wx.ID_ABORT,'&ABOUT','ABOUT THE EDITOR')
         filemenu.AppendSeparator()
-        filemenu.Append(wx.ID_EXIT,u'退出',u'退出程序')
-
-        btn_mea = wx.Button(self,-1,u'打开文件',pos=(300,100),size=(100,-1))
+        menuExit = filemenu.Append(wx.ID_EXIT, 'E&XIT',"EXIT THE PROGRAM")
 
         menuBar = wx.MenuBar()
-        menuBar.Append(filemenu,u"文件")
+        menuBar.Append(filemenu,'&file')
         self.SetMenuBar(menuBar)
-        self.Bind(wx.EVT_MENU,self.OnAbout,menu_about)
-   #     self.Bind(wx.EVT_MENU,self.OnOpen,menu_open)
+
+        self.Bind(wx.EVT_MENU,self.OnOpen,menuOpen)
+        self.Bind(wx.EVT_MENU,self.OnAbout,menuAbout)
+        self.Bind(wx.EVT_MENU,self.OnExit,menuExit)
+
+        self.sizer2 = wx.BoxSizer(wx.HORIZONTAL)
+        self.buttons = []
+        for i in range(0,6):
+            self.buttons.append(wx.Button(self,-1,"Button &" + str(i)))
+            self.sizer2.Add(self.buttons[i],1,wx.SHAPED)
+
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer.Add(self.control,1,wx.EXPAND)
+        self.sizer.Add(self.sizer2,0,wx.GROW)
+
+        self.SetSize(self.sizer)
+        self.SetAutoLayout(True)
+        self.sizer.Fit(self)
         self.Show(True)
 
-    def OnAbout(self,evt):
-        dlg = wx.MessageDialog(self,"A small text editor;","a editor for test")
+    def OnAbout(self,e):
+        dlg = wx.MessageDialog(self,"a samll editor","small",wx.OK)
         dlg.ShowModal()
         dlg.Destroy()
 
-    def OnOpen(self,evt):
-        self.dirname=  ''
-        dlg = wx.FileDialog(self,"choose a file",self.dirname,"","*",wx.OPEN)
+    def OnExit(self,e):
+        self.Close(True)
+
+    def OnOpen(self,e):
+        dlg = wx.FileDialog(self,"Choose a file","","*.*",wx.OPEN)
         if dlg.ShowModal() == wx.ID_OK:
-            self.filename == dlg.getname()
-            self.dirname == dlg.GetDirectory()
-            f=open(os.path.join(self.dirname,self.filename),'r')
+            self.filename = dlg.GetFilename()
+            self.dirname = dlg.GetDirectory()
+            f = open(os.path.join(self.dirname,self.filename),'r')
             self.control.SetValue(f.read())
             f.close()
         dlg.Destroy()
+
+
 
 def main():
     app = wx.App(False)
