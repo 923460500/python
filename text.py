099d93fb266d856dcd59a7e5bf5928bc09f6832d
wat4m <wat4m@foxmail.com>
2020-01-03 17:07:41 +0800

    update

--- text.py
+++ text.py
@@ -4,17 +4,47 @@ import sys
 import os
 
 
-class MainWindow(wx.Frame):
-    def __init__(self, parent):
-        wx.Frame.__init__(self,parent)
-        panel = wx.Panel(self)
-        self.quote = wx.StaticText(panel, label="Your Quote", pos=(20,30))
-        self.Show()
+class MyFrame(wx.Frame):
+    def __init__(self):
+        super().__init__(parent=None, title='窗口', size=(600, 500), pos=(100, 100))
+        panel = wx.Panel(parent=self)
+        statictext = wx.StaticText(parent=panel, label='url:', pos=(20, 20))
+        TextCtrl = wx.TextCtrl(parent=panel, size=(110, 25), pos=(40, 20))  # 添加URL
+        self.savebutton = wx.Button(panel, -1, "save", pos=(155, 20), size=(50, 25))  # 点击保存
+        urlshow = wx.StaticText(parent=panel, label='all url in here:', pos=(20, 50))
+        self.allurl = wx.TextCtrl(parent=panel, pos=(20, 70), size=(200, 350),
+                             style=wx.TE_MULTILINE | wx.TE_READONLY)  # 显示已经保存的url
+
+        startbutton = wx.Button(panel, -1, "start", pos=(225, 220), size=(50, -1))
+
+        statustext = wx.StaticText(parent=panel, label="start status:", pos=(280, 20))
+
+        statusshow = wx.TextCtrl(parent=panel, size=(280, 370), pos=(280, 48), style=wx.TE_MULTILINE | wx.TE_READONLY)
+
+        self.Bind(wx.EVT_BUTTON,self.OnSave,self.savebutton)
+     #   startbutton.Bind(self.OnStart)
+
+    def OnSave(self):
+        with open('url.txt','r') as fp:
+            for i in fp.readlines():
+                self.allurl.AppendText("%s" %i)
+        fp.close()
+
+    def OnStart(self):
+        pass
+class App(wx.App):
+    def OnInit(self):
+        frame = MyFrame()
+        frame.Show()
+        return True
+
+    def OnExit(self):
+        print("exit")
+        return 0
 
 
 def main():
-    app = wx.App(False)
-    panel = MainWindow(None)
+    app = App()
     app.MainLoop()
 
 
