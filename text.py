3d190a96726f564d20a357f3aded74f75a1212d5
wat4m <wat4m@foxmail.com>
2020-01-03 18:26:42 +0800

    update

--- text.py
+++ text.py
@@ -13,25 +13,44 @@ class MyFrame(wx.Frame):
         self.savebutton = wx.Button(panel, -1, "save", pos=(155, 20), size=(50, 25))  # 点击保存
         urlshow = wx.StaticText(parent=panel, label='all url in here:', pos=(20, 50))
         self.allurl = wx.TextCtrl(parent=panel, pos=(20, 70), size=(200, 350),
-                             style=wx.TE_MULTILINE | wx.TE_READONLY)  # 显示已经保存的url
+                                  style=wx.TE_MULTILINE | wx.TE_READONLY)  # 显示已经保存的url
 
-        startbutton = wx.Button(panel, -1, "start", pos=(225, 220), size=(50, -1))
+        self.startbutton = wx.Button(panel, -1, "start", pos=(225, 220), size=(50, -1))
 
         statustext = wx.StaticText(parent=panel, label="start status:", pos=(280, 20))
 
-        statusshow = wx.TextCtrl(parent=panel, size=(280, 370), pos=(280, 48), style=wx.TE_MULTILINE | wx.TE_READONLY)
+        self.statusshow = wx.TextCtrl(parent=panel, size=(280, 370), pos=(280, 48), style=wx.TE_MULTILINE | wx.TE_READONLY)
 
-        self.Bind(wx.EVT_BUTTON,self.OnSave,self.savebutton)
-     #   startbutton.Bind(self.OnStart)
+        self.Bind(wx.EVT_LEFT_DOWN, self.OnSave, self.savebutton)
+        self.Bind(wx.EVT_BUTTON, self.Refresh, self.savebutton)
+        self.Bind(wx.EVT_BUTTON,self.OnStart,self.startbutton)
 
-    def OnSave(self):
+    #   startbutton.Bind(self.OnStart)
+    def OnSave(self, event):
+        print("save")
+        with open('url.txt', 'a') as fp:
+            fp.write('%s' % event.GetString())
+        fp.close()
+        event.Skip()
+
+    def Refresh(self, event):
+        self.allurl.Clear()  # 清空输出
+        with open('url.txt', 'r') as fp:
+            for i in fp.readlines():
+                self.allurl.AppendText("%s" % i)
+        fp.close()
+        event.Skip()
+
+    def OnStart(self,event):
+        self.statusshow.Clear()
         with open('url.txt','r') as fp:
             for i in fp.readlines():
-                self.allurl.AppendText("%s" %i)
+                i = i.rstrip("\n")
+                self.statusshow.AppendText("%s startup ok!\n" % i)
         fp.close()
 
-    def OnStart(self):
-        pass
+    def 
+
 class App(wx.App):
     def OnInit(self):
         frame = MyFrame()
