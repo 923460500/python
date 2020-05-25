cd9c0cf9a9e7b77396c500d57b2ecf336d98373c
Administrator <wat4me@foxmail.com>
2020-05-25 18:52:31 +0800

    4database

--- test.py
+++ test.py
@@ -1,8 +1,20 @@
 # coding:utf-8
 
-a=["creat table asd", "{test varchar(4)","test1 varchar(4)","};"]
+import prometheus_client
+from prometheus_client import Counter
+from prometheus_client.core import CollectorRegistry
+from flask import Response, Flask
 
-for i in a:
-    if ";" in i:
-        print(";")
-    print(i)
+app = Flask(__name__)
+
+requests_total = Counter("request_count", "total request count of the host")
+
+
+@app.route("/met")
+def request_count():
+    requests_total.inc()
+    return Response(prometheus_client.generate_latest(requests_total), mimetype="text/plain")
+
+
+if __name__ == '__main__':
+    app.run("0.0.0.0")
